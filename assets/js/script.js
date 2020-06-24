6d093cc82be3524b1460bca73f432ce281c7f968
Khargol <josephklein3@gmail.com>
2020-06-24 19:34:53 -0400

    start screen style

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Variables
+
+
+//Action Listeners
+
+
+//Functions
+
+//Pre-game sets all values and preps game.
+
+//game start load questions, adjust css to make questions appear, start timer track score
+//Use event listner to get button and validate answer. use object, 4 strings, correct choice boolean.
+
+//while game not done, timer counts down, at quiz end or timer end finish game
+//show stats screen, make text dialog box, save score and initials
